20654479c1b0f84d4fb56ef1a50c49306e440b14
Corbin Kim <gkim26@ncsu.edu>
2026-09-21 01:58:35 -0400

    Add researchInterests to Home component imports

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Home.scss";
-import {site, social} from "../data/site";
+import {site, social, researchInterests} from "../data/site";
 import {timeline, awards, publications} from "../data/resume";
 import {projects} from "../data/projects";
 import Writing from "./Writing";
